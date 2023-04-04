e225be717950d74f114163805198d4bc6f54b778
Maria <pashkovichma@gmail.com>
2023-04-04 14:37:01 +0300

    fix: routes in header

--- vite-project/src/components/header/header.tsx
+++ vite-project/src/components/header/header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './header.css';
+import { Pathes } from '../../pathes/pathes-enum';
 
 const Header: React.FC = () => {
   return (
@@ -8,17 +9,17 @@ const Header: React.FC = () => {
       <nav className="header__navigation">
         <ul className="navigation__list">
           <li className="navigation__item">
-            <NavLink className="navigation__link" to={'/'}>
+            <NavLink className="navigation__link" to={Pathes.Home}>
               Home
             </NavLink>
           </li>
           <li className="navigation__item">
-            <NavLink className="navigation__link" to={'/aboutus'}>
+            <NavLink className="navigation__link" to={Pathes.About_Us}>
               About us
             </NavLink>
           </li>
           <li className="navigation__item">
-            <NavLink className="navigation__link" to={'/form'}>
+            <NavLink className="navigation__link" to={Pathes.Form}>
               Form
             </NavLink>
           </li>
